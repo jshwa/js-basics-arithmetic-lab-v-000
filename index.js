bbd695df36a98eaaaeb05a877db0daf566faf301
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-13 22:42:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ const newID = oldID + 1000000000;
 const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.random() * 20;
 const randomInteger = Math.floor(randomNumber);
-const randomUserId = randomInteger + 10000000000;
+const randomUserID = randomInteger + 1000000000;
